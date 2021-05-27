4abed4bb716706f7721c89ca780f8ac83b1eaccf
Rohan-Shinde-98 <82387052+Rohan-Shinde-98@users.noreply.github.com>
2021-05-27 16:28:40 +0530

    Update che.sh

--- che.sh
+++ che.sh
@@ -31,15 +31,15 @@ spec:
 
     #check for the image name format
     IFS=':'
-    read -ra sep <<< "$CHE_SERVER_CUSTOM_IMAGE"
-    if [ ${#sep[@]} != 2 ]
+    read -ra split <<< "$CHE_SERVER_CUSTOM_IMAGE"
+    if [ ${#split[@]} != 2 ]
     then
       echo "che server image needs to be in format: [server/]imageName:imageTag. Found ${CHE_SERVER_CUSTOM_IMAGE}"
       exit 1
     fi
     unset IFS
-    echo "    cheImage: '${sep[0]}'"
-    echo "    cheImageTag: ${sep[1]}"
+    echo "    cheImage: '${split[0]}'"
+    echo "    cheImageTag: ${split[1]}"
     echo "    cheImagePullPolicy: IfNotPresent"
 	fi
   )"
@@ -55,12 +55,13 @@ chectl server:deploy --listr-renderer=verbose --platform=minikube --che-operator
 
 
 echo "Eclipse Che [sets che-url]..."
-kubectl get ingress che -n eclipse-che -o jsonpath='{.spec.rules[0].host}' 
-# 3 steps remaining
+getCheIngressProcess=$(kubectl get ingress che -n eclipse-che -o jsonpath='{.spec.rules[0].host}')
+cheHostName=$(echo "$getCheIngressProcess"|sed "s/'//g")
+cheUrl="https://${cheHostName}"
 
 echo "Eclipse Che [sets che-token]..."
-kubectl get ingress/keycloak -n eclipse-che -o jsonpath='{.spec.rules[0].host}'
-# some steps are remaining
+getIngressProcess=$(kubectl get ingress/keycloak -n eclipse-che -o jsonpath='{.spec.rules[0].host}')
+keycloakBaseUrl=$(echo "$getIngressProcess"|sed "s/'//g")
 curl -k -X POST $keycloakUrl -H Content-Type: application/x-www-form-urlencoded -d username=admin -d password=admin -d grant_type=password -d client_id=che-public
 
 echo "Eclipse Che [login]..."
